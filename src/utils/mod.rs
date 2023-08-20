aafe0a0c686d5d63f79a8bf96f992e96ff056757
Umatriz <artsharyi@gmail.com>
2023-08-20 19:36:23 +0300

    feat: `parse_path` util

---
+++ src/utils/mod.rs
@@ -0,0 +1,27 @@
+use tokio::{io::AsyncReadExt, net::TcpStream};
+
+use crate::prelude::Result;
+
+use crate::prelude::Error;
+
+pub async fn parse_path(mut stream: TcpStream) -> Result<Vec<String>> {
+    let mut buf = vec![0; 1024];
+    let m = stream.read(&mut buf).await.unwrap();
+
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+
+    let res = req.parse(&buf[..m]).unwrap();
+
+    if res.is_partial() {
+        match req.path {
+            Some(path) => {
+                let segments = path.split('/').map(|s| s.to_string()).collect::<Vec<_>>();
+                Ok(segments)
+            }
+            None => Err(Error::PathParse),
+        }
+    } else {
+        Err(Error::PathParse)
+    }
+}
