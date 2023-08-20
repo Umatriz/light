e2660e62086560e1150c2174eb637a859645100c
Umatriz <artsharyi@gmail.com>
2023-08-20 19:37:03 +0300

    feat: add `Cluster`

--- src/clusters/mod.rs
+++ src/clusters/mod.rs
@@ -1,2 +1,20 @@
-#[derive(Clone, Default, Debug)]
-pub struct Cluster;
+use std::rc::Rc;
+
+use tokio::net::TcpStream;
+
+use self::service::Service;
+
+pub mod service;
+
+pub async fn check_cluster() {}
+
+#[derive(Default, Debug)]
+pub struct Cluster {
+    root_path: String,
+    services: Vec<Box<dyn Service>>,
+    clusters: Vec<Cluster>,
+}
+
+impl Cluster {
+    pub fn process(stream: TcpStream) {}
+}
