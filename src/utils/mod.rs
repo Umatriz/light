a04486b5b81827420f1ede552371e4bcd1ff6501
Umatriz <artsharyi@gmail.com>
2023-08-23 18:45:29 +0300

    fix: `parse_path` fix

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,23 +1,24 @@
+use httparse::Request;
 use tokio::{io::AsyncReadExt, net::TcpStream};
 
 use crate::prelude::Result;
 
 use crate::prelude::error::*;
 
-pub async fn parse_path(mut stream: TcpStream) -> Result<Vec<String>> {
-    let mut buf = vec![0; 1024];
-    let m = stream.read(&mut buf).await.unwrap();
+pub async fn parse_path<'a>(
+    buf: &'a [u8],
+    // 1.5 days spent to understand that i can just pass `headers` as an argument
+    headers: &'a mut [httparse::Header<'a>; 16],
+) -> Result<(Vec<String>, Request<'a, 'a>)> {
+    let mut req = httparse::Request::new(headers);
 
-    let mut headers = [httparse::EMPTY_HEADER; 16];
-    let mut req = httparse::Request::new(&mut headers);
-
-    let res = req.parse(&buf[..m]).unwrap();
+    let res = req.parse(buf).unwrap();
 
     if res.is_partial() {
         match req.path {
             Some(path) => {
                 let segments = path.split('/').map(|s| s.to_string()).collect::<Vec<_>>();
-                Ok(segments)
+                Ok((segments, req))
             }
             None => Err(Error::Light {
                 message: "Error while path parsing".into(),
