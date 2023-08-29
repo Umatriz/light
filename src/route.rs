6b7dfad690cd2c6986653bf2bee9ac81fb634cdd
Umatriz <artsharyi@gmail.com>
2023-08-29 18:25:58 +0300

    Update route.rs

--- src/route.rs
+++ src/route.rs
@@ -1,50 +1,54 @@
-use std::collections::BTreeMap;
-use std::sync::{Arc, RwLock, Weak};
+use std::{
+    fmt::Debug,
+    sync::{Arc, RwLock, Weak},
+};
 
-// #[derive(Debug, Default)]
-// struct Tree<T> {
-//     tree: Vec<Arc<Node<T>>>,
-// }
+use crate::prelude::Wrapper;
 
-// #[derive(Debug)]
-// struct Node<T> {
-//     payload: T,
-//     parent: Arc<RwLock<Weak<Node<T>>>>,
-//     children: Arc<Vec<RwLock<Arc<Node<T>>>>>,
-// }
-
-#[derive(Debug, Default)]
-struct Tree<T> {
-    tree: Vec<Arc<RwLock<Node<T>>>>,
+struct Node<T: Clone> {
+    inner: Arc<RwLock<NodeData<T>>>,
 }
 
 #[derive(Debug)]
-struct Node<T> {
+struct NodeData<T: Clone> {
     payload: T,
-    parent: Weak<RwLock<Node<T>>>,
-    children: Vec<Arc<RwLock<Node<T>>>>,
+    parent: Weak<RwLock<NodeData<T>>>,
+    children: Vec<Arc<RwLock<NodeData<T>>>>,
+}
+
+impl<T: Clone> Node<T> {
+    pub fn new(payload: T) -> Self {
+        Self {
+            inner: Arc::new(RwLock::new(NodeData {
+                payload,
+                parent: Weak::new(),
+                children: vec![],
+            })),
+        }
+    }
+}
+
+impl<T: Clone + Debug> Node<T> {
+    pub fn add_children(&mut self, node_data: Node<T>) {
+        let binding: Arc<RwLock<NodeData<T>>> = node_data.inner.clone();
+        let mut r_node_data = binding.write().unwrap();
+        r_node_data.parent = Arc::downgrade(&self.inner);
+
+        let mut children = self.inner.read().unwrap().children.clone();
+        children.push(node_data.inner);
+
+        let binding = &self.inner.clone();
+        let mut self_locked = binding.write().unwrap();
+
+        self_locked.children = children;
+    }
 }
 
 #[test]
-fn node_test() {
-    let node = Arc::new(RwLock::new(Node::<i32> {
-        payload: 1,
-        parent: Weak::new(),
-        children: vec![],
-    }));
+fn node_data_test() {
+    let mut node_data = Node::new("payload");
+    let c_node_data = Node::new("c");
+    node_data.add_children(c_node_data);
 
-    let tree = Tree {
-        tree: vec![Arc::new(RwLock::new(Node::<i32> {
-            payload: 0,
-            parent: Weak::new(),
-            children: vec![node.clone()],
-        }))],
-    };
-
-    let binding = node;
-    let mut r = binding.write().unwrap();
-
-    r.parent = Arc::downgrade(&tree.tree[0].clone());
-
-    std::thread::spawn(move || println!("{:#?}", tree));
+    println!("{:#?}", node_data.inner.clone());
 }
